86750518f193d625f1c85d91b2cd012d55d355f9
theajaypee <110385542+theajaypee@users.noreply.github.com>
2022-08-19 17:15:52 +0800

    mobile section

---
+++ starter/main.js
@@ -0,0 +1,17 @@
+
+
+
+// ESS funtion
+var hamburger = document.querySelector('.hamburger-menu')
+var mobilelinks = document.querySelector('.mobile__link-holder')
+var backdrop = document.querySelector('.backdrop')
+
+
+
+
+
+function Addclass() {
+    hamburger.classList.toggle('show')  
+    mobilelinks.classList.toggle('showlinks')
+    backdrop.classList.toggle('showlinks')
+}
